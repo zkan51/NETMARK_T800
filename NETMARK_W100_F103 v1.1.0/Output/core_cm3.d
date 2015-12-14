ab0f99772439e0e5bfc4a32d836970b65a2f3ea6
xieyoub <596931725@qq.com>
2015-12-14 18:36:48 +0800

    v1.1.3 2015.12.12 修改AD采样 修改算法

--- NETMARK_W100_F103 v1.1.0/Output/core_cm3.d
+++
@@ -1,2 +0,0 @@
-..\Output\core_cm3.o: ..\CMSIS\core_cm3.c
-..\Output\core_cm3.o: F:\keil4\ARM\ARMCC\bin\..\include\stdint.h
---
+++ NETMARK_W100_F103 v1.1.3/Source/gps.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "math.h"
+
+
+static u8 t_convert=0;   //记录收到有效位置信息的次数
+
+int GPS_RMC_Parse(char *line, GPS_INFO *GPS)
+{	
+	
+	
+	char *buf;
+	u8 ch, status;
+	u16 time_n,delta_time;
+	
+	if (buf!=NULL)  //($GPRMC)
+	{	
+		if (status == 'A')  //如果数据有效，则分析
+		{	
+			t_convert++;
+			
+			//航速  time_o的backup挪作他用
+			if(GetComma( 7, buf)!=0){
+				sog = Get_Double_Number_sog(&buf[GetComma( 7, buf)]);
+			}
+			
+			//航向
+//			Read_Flash_Cogsel();				
+			
+//			Write_GPS_Info();
+				
+			TIM_Cmd(TIM2, DISABLE);
+							
+			if(charging_flag == off)
+			{
+				PWR_WakeUpPinCmd(ENABLE);
+				PWR_EnterSTANDBYMode();
+			}	
+		}
+	}
+	
+	return 1;
+}
