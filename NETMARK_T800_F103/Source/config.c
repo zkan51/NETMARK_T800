7ea02316eb8f3e33309718d2886f484989b4e87d
xieyoub <596931725@qq.com>
2015-12-28 20:34:32 +0800

    v1.1.6 2015.12.28 红灯控制脚改为PB7

--- NETMARK_T800_F103/Source/config.c
+++ NETMARK_T800_F103/Source/config.c
@@ -23,7 +23,7 @@ void GPIO_Configuration(void)
 //   	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 //   	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-  	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;    //LED RED
+  	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;    //LED RED
   	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   	GPIO_Init(GPIOB, &GPIO_InitStructure);
